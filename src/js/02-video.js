7c1e58868d6e48ca0e7999314dd781b9cb255292
Svitlana Samoliuk <101995368+Svetlana1208@users.noreply.github.com>
2023-01-04 23:47:30 +0200

    added task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+    import throttle from 'lodash.throttle';
+    
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', throttle(onSaveTime, 1000)); 
+    function onSaveTime(evt) {
+        localStorage.setItem("videoplayer-current-time", evt.seconds)
+    };
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
 

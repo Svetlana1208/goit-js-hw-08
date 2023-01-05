ccbe4367ede6b55f5c2aefd8dd7ec69dd05bbff0
Svitlana Samoliuk <101995368+Svetlana1208@users.noreply.github.com>
2023-01-05 17:55:30 +0200

    added task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form  textarea'),
+  };
+  
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onDataInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    console.log(formData);
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+function onDataInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
+function populateTextarea () {
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    const ggg = JSON.parse(savedMessage);
+
+    if (savedMessage) {
+        refs.input.value = ggg.email;
+        refs.textarea.value = ggg.message;
+      }
+}
